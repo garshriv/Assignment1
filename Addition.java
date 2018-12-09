3e27bde9df3ad5f20cda7da278da33e4fc6add68
garshriv <45705346+garshriv@users.noreply.github.com>
2018-12-09 13:34:09 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,28 @@
+public class Addition {
+  
+  public static void main(String args[]) {
+ 
+       int x=10;
+       
+       int y=50;
+
+        
+       System.out.println("Sum of x+y = " + add(x,y));
+    
+  }
+    
+  public static int add(int a, int b){
+
+        if(b == 0) return a;
+
+        int sum = a ^ b; //SUM of two integer is A XOR B
+
+        int carry = (a & b) << 1;  //CARRY of two integer is A AND B
+
+        return add(sum, carry);
+
+    }
+
+
+
+}
---
+++ Expressions.java
@@ -0,0 +1,32 @@
+public class Expressions{
+
+    public static void main(String args[]) {
+
+        int a=20;
+
+        int b=10;
+
+
+
+            b = a-- - --a;
+
+        int c = a--;
+
+        int d = a>>2;
+
+        int e = a&b;
+
+
+        System.out.println("a  = " + a);
+
+        System.out.println("b  = " + b);
+
+        System.out.println("c  = " + c);
+
+        System.out.println("d  = " + d);
+
+        System.out.println("e  = " + e);
+
+    }
+
+}
